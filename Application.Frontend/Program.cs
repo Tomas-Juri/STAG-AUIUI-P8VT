b5f08166c79d1cad25b845adde069e81ba2f5a98
Juřička, Tomáš <tomas.juricka@outlook.cz>
2022-02-09 12:57:49 +0100

    Add project files.

---
+++ Application.Frontend/Program.cs
@@ -0,0 +1,15 @@
+using Application.Frontend;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var apiUrl = builder.Configuration["ApiUrl"];
+if (apiUrl == null)
+    throw new ApplicationException("ApiUrl not defined in appsettings.json");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
+
+await builder.Build().RunAsync();
---
+++ _docs/index.md
@@ -0,0 +1,6 @@
+# 
+
+- Install [.NET6 SDK](https://dotnet.microsoft.com/en-us/download/dotnet/6.0)
+- Install [Visual Code](https://code.visualstudio.com/) with following extensions
+    - [C#](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csharp)
+    - [Microsoft.AspNetCore.Razor.VSCode.BlazorWasmDebuggingExtension](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.blazorwasm-companion)
